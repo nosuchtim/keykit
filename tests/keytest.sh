371da98bf5b9d64796fd024d9f9934b5ac863f9b
Peter Barada <5862219+pbarada@users.noreply.github.com>
2023-10-16 10:54:36 -0400

    Pass ARGS to each test
    Pass ${ARGS} to each test to run - in this case to set
    Debugmemscribble to non-zero which causes dbgmyallocate to scribble
    over freshly allocated memory.

--- tests/keytest.sh
+++ tests/keytest.sh
@@ -6,45 +6,45 @@ echo "Arg passed to lowkey: '${ARGS}'"
 
 echo Running ack test ...
 
-KEYPATH=../lib ../bin/lowkey ack.k > ack.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} ack.k > ack.out
 diff ack.out ack.sav
 
 echo Running attrib test ...
-KEYPATH=../lib ../bin/lowkey attrib.k > attrib.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} attrib.k > attrib.out
 diff attrib.out attrib.sav
 
 echo Running bigtest test ...
-KEYPATH=../lib ../bin/lowkey bigtest.k > bigtest.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} bigtest.k > bigtest.out
 diff bigtest.out bigtest.sav
 
 echo Running control test ...
-KEYPATH=../lib ../bin/lowkey control.k > control.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} control.k > control.out
 diff control.out control.sav
 
 echo Running dot test ...
-KEYPATH=../lib ../bin/lowkey dot.k > dot.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} dot.k > dot.out
 diff dot.out dot.sav
 
 echo Running fork test ...
-KEYPATH=../lib ../bin/lowkey fork.k > fork.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} fork.k > fork.out
 diff fork.out fork.sav
 
 echo Running more test ...
-KEYPATH=../lib ../bin/lowkey more.k > more.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} more.k > more.out
 diff more.out more.sav
 
 echo Running multi test ...
-KEYPATH=../lib ../bin/lowkey multi.k > multi.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} multi.k > multi.out
 diff multi.out multi.sav
 
 echo Running obj test ...
-KEYPATH=../lib ../bin/lowkey obj.k > obj.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} obj.k > obj.out
 diff obj.out obj.sav
 
 echo Running ptr test ...
-KEYPATH=../lib ../bin/lowkey ptr.k > ptr.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} ptr.k > ptr.out
 diff ptr.out ptr.sav
 
 echo Running logical test ...
-KEYPATH=../lib ../bin/lowkey logical.k > logical.out
+KEYPATH=../lib ../bin/lowkey ${ARGS} logical.k > logical.out
 diff logical.out logical.sav
