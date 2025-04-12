33640db79a84c59de621ac5759e34fbce5faa017
spacepalette <spacepalette@gmail.com>
2025-04-12 13:10:08 -0700

    tweak for visual studio 2022

--- setwindows.bat
+++ setwindows.bat
@@ -1,1 +1,1 @@
-%comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+%comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
